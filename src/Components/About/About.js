b919a489c7413060e7334dd3a82cfd2eb4fdbe96
Koushik <koushik173>
2022-11-24 07:31:12 +0600

    dur vala lage na

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import { Card, CardGroup, Row } from 'react-bootstrap';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-           <p>This is about</p> 
+        <div className='about-con'>
+           <Row>
+            <p className='top'>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</p>
+
+            <p className='midle'>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</p>
+
+            <p className='down'>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            </Row> 
+
+            <Row className='card'>
+                <CardGroup>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in
+                            to additional content. This content is a little bit longer.
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This card has supporting text below as a natural lead-in to
+                            additional content.{' '}
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in
+                            to additional content. This card has even longer content than the
+                            first to show that equal height action.
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup>
+            </Row>
         </div>
     );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import { Form } from 'react-router-dom';
+import { Button, Row, Container } from 'react-bootstrap';
+import { Form, Link } from 'react-router-dom';
 import './Home.css'
 
 const Home = () => {
     return (
         <div className='home-container'>
-            <Container>
-            <Row>
-                <Col className='welcome'>
-                    <h1>WELCOME To Roy Restro</h1>
-                    <h4>Delivering great food for more than 18 years!</h4>
-                    <button> Our Menu</button>
-                </Col>
-                <Col>2 of 3</Col>
-                </Row>
-            </Container>
+            <div className='welcome'>
+                <h1>WELCOME To <span className='roy'>Roy Restro</span></h1>
+                <h4>Delivering great food for more than 18 years!</h4><br />
+                <Link className='menU' to="/menu"> Menu</Link>
+                <Link className='menU'> Book A Table</Link>
+            </div>
+            <div>
+                
+            </div>
         </div>
     );
 };
